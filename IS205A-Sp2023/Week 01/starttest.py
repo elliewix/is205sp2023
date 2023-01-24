0c6c516d3eec5add7a218ee7bf70ff4c243e2ba7
Elizabeth Wickes <elliewix@gmail.com>
2023-01-24 09:27:50 -0600

    adding first week stuff

---
+++ IS205A-Sp2023/Week 01/starttest.py
@@ -0,0 +1,7 @@
+print("hello humans")
+print("super not a robot")
+print("1")
+print("banana")
+
+bananas = 5
+print(bananas)
---
+++ IS205B-Sp2023/week 01/starttest.py
@@ -0,0 +1,6 @@
+print("hello humans")
+print("super not a computer")
+print("bananas")
+
+print(1 + 1)
+print("1 + 1")
