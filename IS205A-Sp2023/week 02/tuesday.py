2f9f7ab185f0e5358222ed5045c3d7eb08929a49
Elizabeth Wickes <elliewix@gmail.com>
2023-01-26 08:44:19 -0600

    tuesday

---
+++ IS205A-Sp2023/week 02/tuesday.py
@@ -0,0 +1,13 @@
+print(4)
+print('4')
+
+print(4 * 2)
+print('4' * 3)
+# print('4' * '4') # here we get an error
+
+text = '4'
+# print(text * 2)
+
+repeated = text * 2
+print("hello from line 12:", repeated)
+
---
+++ IS205B-Sp2023/week 02/tuesday.py
@@ -0,0 +1,14 @@
+print(4)
+print('4')
+
+print(4 * 2)
+print('4' * 2)
+print('4' * 3)
+# print('4' * '4') # this gives an error
+4 * 2 # does the thing but doesn't print
+
+text = '4'
+print(text * 2)
+
+result = text * 2
+print(result)
